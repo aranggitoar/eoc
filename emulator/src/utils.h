be1000eb6c753b008e56a25a4c09f13f456d50b9
Aranggi J. Toar <at@aranggitoar.com>
2024-03-18 14:17:07 +0700

    Org: Clarify function names.

--- emulator/src/utils.h
+++ emulator/src/utils.h
@@ -3,12 +3,12 @@
 
 #include <std.h>
 
-// CHeck BIt, Print All BIts, Set BIt, CLear BIt, Toggle BIt.
 // Mostly used for reference as it's shorter to type as is.
-#define CHBI(b, i) (b >> i) & 1 // i == 0 is little endian LSB
-#define PABI(b, sz) for (int i = sz-1; i >= 0; i--) printf(i == 0 ? "%d\n" : "%d ", CHBI(b, i));
-#define SBI(b, i) b | 1 << i // i == 0 is little endian MSB
-#define CLBI(b, i) b & ~(1 << i) // endianness same as SBI
-#define TBI(b, i) b ^ (1 << i) // endianness same as SBI
+// Written as #define so it accepts any number type.
+#define CHECK_BIT(b, i) (b >> i) & 1 // i == 0 is little endian LSB
+#define PRINT_ALL_BITS(b, sz) for (int i = sz-1; i >= 0; i--) printf(i == 0 ? "%d\n" : "%d ", CHECK_BIT(b, i));
+#define SET_BIT(b, i) b | 1 << i // i == 0 is little endian MSB
+#define CLEAR_BIT(b, i) b & ~(1 << i) // endianness same as SBI
+#define TOGGLE_BIT(b, i) b ^ (1 << i) // endianness same as SBI
 
 #endif /* UTILS_H_INCLUDED */
